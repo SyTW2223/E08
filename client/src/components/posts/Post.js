ddf673d1c0ba6b77f195b27637a4451c5edd51ab
Vlatko Marchan <alu0101321141@ull.edu.es>
2023-01-19 15:25:56 +0000

    nuevo estilo de posts

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Paper, Box, Typography, Grid, IconButton } from "@mui/material";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Paper, Box, Typography, Grid, IconButton, Avatar } from "@mui/material";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
@@ -18,6 +17,33 @@ const ButtonDeleteHandler = (id, accountDelete, dispatch) => {
     dispatch(deletePost(id, accountDelete));
 }
 
+function stringToColor(string) {
+    let hash = 0;
+    let i;
+  
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    return color;
+  }
+  
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+        color: "white"
+      },
+      children: `${name[0].toUpperCase()}`,
+    };
+  }
+
 
 export const Post = ({ id, title, accountName, content, index, tags }) => {
     const dispatch = useDispatch();
@@ -26,53 +52,63 @@ export const Post = ({ id, title, accountName, content, index, tags }) => {
     const { user: currentUser } = useSelector(state => state.auth);
 
     return (
-        <Paper elevation={3} sx={{ padding: 2, margin: 2 }}>
-            {isLoggedIn && accountName === currentUser.accountName 
-            ? <Box display="flex" alignItems="right" float="right" justifyContent="right">
-                <IconButton aria-label="delete" onClick={() => ButtonDeleteHandler(id, currentUser.accountName , dispatch)} sx={{ justifyContent: "right" }}>
-                    <DeleteForeverIcon />
-                </IconButton>
-            </Box>
-            : null}
-            <Grid container spacing={2}>
-                <Grid item>
-                    <AccountCircleIcon size="3em" sx={{ marginTop: "6px" }} />
+        <Paper elevation={3} sx={{ padding: 3, margin: 2 }}>
+            <Grid container alignItems="center">
+                <Grid container md={1} xs={1} justifyContent="center">
+                    <Avatar {...stringAvatar(accountName)} />
                 </Grid>
-                <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                        <Grid item>
-                            <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-                                {title}
-                            </Typography>
-                            <Typography variant="subtitle2">
-                                {accountName}
-                            </Typography>
-                            <hr />
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="body2">
-                                {content}
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <hr />
-                            <Box display="flex" flexDirection="row" alignItems="left" justifyContent="left">
-                                <Typography variant="body2">
-                                    Tags: {String(tags).replace(/,/g, ", ")}
-                                </Typography>
-                            </Box>
-                            <Box display="flex" flexDirection="row" alignItems="right" justifyContent="right">
-                                <Typography variant="subtitle1" fontSize="1.5em">
-                                    {currentPosts[index].likesFromAccounts.length}
-                                </Typography>
-                                <IconButton
-                                    aria-label="like"
-                                    onClick={() => ButtonLikeHandler(id, currentUser.accountName , dispatch)}>
-                                    <FavoriteBorderIcon />
-                                </IconButton>
-                            </Box>
-                        </Grid>
-                    </Grid>
+                <Grid item md={10} xs={10}>
+                    <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                        {title}
+                    </Typography>
+                    <Typography variant="subtitle2">
+                        by: <b>{accountName}</b>
+                    </Typography>
+                </Grid>
+                <Grid item md={1} xs={1}>
+                    {isLoggedIn && accountName === currentUser.accountName
+                        ? <Box display="flex" alignItems="right" float="right" justifyContent="right">
+                            <IconButton aria-label="delete" onClick={() => ButtonDeleteHandler(id, currentUser.accountName, dispatch)} sx={{ justifyContent: "right" }}>
+                                <DeleteForeverIcon />
+                            </IconButton>
+                        </Box>
+                        : null}
+                </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+                <Grid item md={12} xs={12}>
+                    <Typography  sx={{marginLeft:"2em", marginRight:"2em", marginTop:"1em", marginBottom:"1em"}}>
+                        <hr />
+                    </Typography>    
+                    <Box display="flex" alignItems="left" justifyContent="left" marginLeft="2em" marginRight="2em">
+                        <Typography variant="body2">
+                            {content}
+                        </Typography>
+                    </Box>
+                    <Typography  sx={{marginLeft:"2em", marginRight:"2em", marginTop:"1em", marginBottom:"1em"}}>
+                        <hr />
+                    </Typography> 
+                </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+                <Grid item md={11} xs={9}>
+                    <Box display="flex" alignItems="center" justifyContent="left" marginLeft="2em" marginRight="2em">
+                        <Typography variant="body2">
+                            Tags: {String(tags).replace(/,/g, ", ")}
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item md={1} xs={2}>
+                    <Box display="flex" flexDirection="row" alignItems="center" justifyContent="center">
+                        <Typography variant="subtitle1" fontSize="1em">
+                            {currentPosts[index].likesFromAccounts.length}
+                        </Typography>
+                        <IconButton
+                            aria-label="like"
+                            onClick={() => ButtonLikeHandler(id, currentUser.accountName, dispatch)}>
+                            <FavoriteBorderIcon size="1em" />
+                        </IconButton>
+                    </Box>
                 </Grid>
             </Grid>
         </Paper>
