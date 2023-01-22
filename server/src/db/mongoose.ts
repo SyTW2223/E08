d92083dc880a7b2ca524559f2ae85bcb48c37307
Vlatko Marchan <alu0101321141@ull.edu.es>
2023-01-22 12:07:03 +0000

    prueba 6.0

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -2,15 +2,21 @@ require('dotenv').config();
 import * as mongoose from 'mongoose';
 
 
-const { MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV } = process.env
+const { MONGO_DB_URI, MONGO_DB_URI_TEST, MONGO_DB_URI_COV ,NODE_ENV } = process.env
 
 /**
  * If the enviroment variable is not established,
  * connects to the url
  */
-const connectionUrl = NODE_ENV === 'test'
-  ? MONGO_DB_URI_TEST
-  : MONGO_DB_URI;
+let connectionUrl = ""
+
+if (NODE_ENV === 'test') {
+  connectionUrl = MONGO_DB_URI_TEST as string;
+} else if (NODE_ENV === 'coverage') {
+  connectionUrl = MONGO_DB_URI_COV as string;
+} else {
+  connectionUrl = MONGO_DB_URI as string;
+}
 
 /**
  * Connects to the Mongo server
